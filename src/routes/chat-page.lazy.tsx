bcdec4e8ca668e5a3cae0bc0782acb18e5052c95
Marcin Krawczyk <marckraw@icloud.com>
2025-01-25 09:48:11 +0100

    fix: fix ChatPage type error, add command components from shadcn

--- src/routes/chat-page.lazy.tsx
+++ src/routes/chat-page.lazy.tsx
@@ -1,15 +1,15 @@
-import React from 'react'
-import { ChatView } from '../ui/components/ChatView/ChatView'
-import { createLazyFileRoute } from '@tanstack/react-router'
-
-export const Route = createLazyFileRoute('/chat-page')({
-  component: ChatPage,
-})
+import React from "react";
+import { ChatView } from "../ui/components/ChatView/ChatView";
+import { createLazyFileRoute } from "@tanstack/react-router";
 
 export const ChatPage = () => {
   return (
     <>
       <ChatView />
     </>
-  )
-}
+  );
+};
+
+export const Route = createLazyFileRoute("/chat-page")({
+  component: ChatPage,
+});
