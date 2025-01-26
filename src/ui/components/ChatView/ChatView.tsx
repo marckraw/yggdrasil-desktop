9031dbd880620a5096dd20760da95fd45cb9473e
Marcin Krawczyk <marckraw@icloud.com>
2025-01-26 11:15:42 +0000

    fix: make sidebar wider

--- src/ui/components/ChatView/ChatView.tsx
+++ src/ui/components/ChatView/ChatView.tsx
@@ -42,7 +42,7 @@ export const ChatView = () => {
           </Button>
           {/* Sliding Sidebar Panel */}
           <div
-            className={`fixed top-0 left-0 h-full w-64 bg-background border-r p-4 transform transition-transform duration-300 ease-in-out z-40 
+            className={`fixed top-0 left-0 h-full w-96 bg-background border-r p-4 transform transition-transform duration-300 ease-in-out z-40 
             ${isSidebarOpen ? "translate-x-0" : "-translate-x-full"}`}
           >
             <Button
